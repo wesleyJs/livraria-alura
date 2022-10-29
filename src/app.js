b6c1fc1ad25a04ddf32e2f730424a3a5c4defa23
wesley viana martins <wesley.vmartins.js@gmail.com>
2022-10-29 08:43:57 -0400

    correção

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import db from "./config/dbConnect.js";
-import routes from "./Routes/index.js";
+import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, "erro de conexão"));
 db.once("open", () => {
